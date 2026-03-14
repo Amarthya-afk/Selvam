e68ff68a0cd3ef497dafb0eafe3bdaf41754010e
Amarthya-afk <cb.sc.u4cse23003@cb.students.amrita.edu>
2026-03-14 14:03:14 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ dotenv.config();    // load .env variables
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
   credentials: true
 }));
 
